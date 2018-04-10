8b62b185d2d6739e5db53361e748dd39fbb256da
Gioele Minardi <gioelem3@gmail.com>
2018-04-10 20:14:11 +0200

    AnimatedSprite func declarations

--- Cavestory/AnimatedSprite.cpp
+++ Cavestory/AnimatedSprite.cpp
@@ -1,11 +1,20 @@
 #include "AnimatedSprite.h"
+#include "Graphics.h"
 
 AnimatedSprite::AnimatedSprite()
 {
 }
 
-AnimatedSprite::AnimatedSprite(Graphics & graphics, const std::string & filePath, int sourceX, int sourceY, int width, int height, int posX, int posY, float TimeToUpdate)
+AnimatedSprite::AnimatedSprite(Graphics & graphics, const std::string & filePath, int sourceX, int sourceY, int width, int height, int posX, int posY, float timeToUpdate) :
+	Sprite(graphics, filePath, sourceX, sourceY, width, height, posX, posY),
+	_frameIndex(0),
+	_timeElapsed(0),
+	_visible(true),
+	_timeToUpdate(timeToUpdate),
+	_currentAnimation(""),
+	_currentAnimationOnce(false)
 {
+
 }
 
 AnimatedSprite::~AnimatedSprite()
@@ -25,5 +34,26 @@ void AnimatedSprite::Draw(Graphics & graphics, int x, int y)
 }
 
 void AnimatedSprite::SetupAnimations()
+{
+}
+
+void AnimatedSprite::AddAnimation(int frames, int x, int y, std::string name, int width, int height, Vector2 offset)
+{
+
+}
+
+void AnimatedSprite::ResetAnimations()
+{
+}
+
+void AnimatedSprite::StopAnimation()
+{
+}
+
+void AnimatedSprite::SetVisible(bool visible)
+{
+}
+
+void AnimatedSprite::AnimationDone(std::string currentAnimation)
 {
 }
--- Cavestory/AnimatedSprite.h
+++ Cavestory/AnimatedSprite.h
@@ -11,12 +11,12 @@ class Graphics;
 /* AnimatedSprite
 	Holds Logic for animating sprites
 */
-class AnimatedSprite :  public Sprite
+class AnimatedSprite : public Sprite
 {
 public:
 	AnimatedSprite();
 	AnimatedSprite(Graphics &graphics, const std::string &filePath, int sourceX, int sourceY, int width, int height,
-		int posX, int posY, float TimeToUpdate);
+		int posX, int posY, float timeToUpdate);
 	virtual ~AnimatedSprite();
 
 	// Play animation if it's not already playing
@@ -30,8 +30,27 @@ public:
 
 	// Sets up all animations for a sprite
 	virtual void SetupAnimations();
+protected:
+	double _timeToUpdate;
+	std::string _currentAnimation;
+	bool _currentAnimationOnce;
+
+	void AddAnimation(int frames, int x, int y, std::string name, int width, int height, Vector2 offset);
+
+	void ResetAnimations();
+
+	void StopAnimation();
+
+	void SetVisible(bool visible);
+
+	virtual void AnimationDone(std::string currentAnimation);
 
 private:
 	std::map<std::string, std::vector<SDL_Rect>> _animations;
+	std::map<std::string, Vector2> _offsets;
+
+	int _frameIndex;
+	double _timeElapsed;
+	bool _visible;
 };
 
